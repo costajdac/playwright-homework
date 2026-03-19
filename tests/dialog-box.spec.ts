7759142805f61179393e119859ddf64177f58da5
Joana Costa <costa.joanad@gmail.com>
2026-03-19 10:26:49 -0700

    Address PR comments

--- tests/dialog-box.spec.ts
+++ tests/dialog-box.spec.ts
@@ -17,7 +17,7 @@ test.describe('Dialog Boxes', () => {
         
         // 4. Add assertions of "New Pet Type" section title, "Name" header for the input field and the input field is visible
         await expect(page.getByRole('heading', { name: 'New Pet Type' })).toBeVisible()
-        await expect(page.locator('label')).toBeVisible()
+        await expect(page.locator('label')).toHaveText('Name')
         const petNameInput = page.locator('#name')
         await expect(petNameInput).toBeVisible()
 
@@ -26,7 +26,7 @@ test.describe('Dialog Boxes', () => {
         await page.getByRole('button', {name:'Save'}).click()
 
         // 6. Add an assertion that the last item in the list of pet types has the value of "pig"
-        await expect(page.locator('input[name="pettype_name"]').last()).toHaveValue('pig')
+        await expect(page.locator('[name="pettype_name"]').last()).toHaveValue('pig')
 
         // 7. Click on the "Delete" button for the "pig" pet type
         // 8. Add an assertion to validate the message of the dialog box "Delete the pet type?"
@@ -38,6 +38,7 @@ test.describe('Dialog Boxes', () => {
         await page.getByRole('row', {name:'pig'}).getByRole('button', { name: 'Delete' }).click()
 
         // 10. Add an assertion that the last item in the list of pet types is not the "pig"
+        await page.waitForResponse(response => response.url().includes('/pettypes'))
         await expect(page.locator('input[name="pettype_name"]').last()).not.toHaveValue('pig')
     })
 })
